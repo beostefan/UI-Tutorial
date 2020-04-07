e84ada0db36250ecaf5cb0a30a7c6651cdab2fc1
beostefan <48043002+beostefan@users.noreply.github.com>
2020-04-07 17:48:48 +0200

    Add reselect selector

--- src/components/ItemGrid.js
+++ src/components/ItemGrid.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Table, Button } from "antd";
 import Counter from "./Counter";
 import { useSelector } from "react-redux";
+import { getTodos, getAmountOfTodos } from "../redux/selectors/todoList";
 
 const { Column } = Table;
 
 const ItemGrid = () => {
-  const data = useSelector(state => state.todoList.todos);
+  const data = useSelector(getTodos),
+    amount = useSelector(getAmountOfTodos);
   return (
     <div>
-      <Counter amount={data.length} />
+      <Counter amount={amount} />
       <Table dataSource={data} className="ItemGrid" rowKey="id">
         <Column
           title="Completed"
---
+++ src/redux/selectors/todoList.js
@@ -0,0 +1,13 @@
+import { createSelector } from "reselect";
+
+const getTodosInternal = state => state.todoList.todos;
+
+export const getAmountOfTodos = createSelector(
+  getTodosInternal,
+  todos => todos.length
+);
+
+export const getTodos = createSelector(
+  getTodosInternal,
+  todos => todos
+);
