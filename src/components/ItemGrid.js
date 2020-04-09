cf8b70ca6884fa9788f860ca32185efc1b4e8b47
Stefan Schmidt <schmidt@beontra.com>
2020-04-09 20:50:47 +0200

    Move axios request to TodoList component

--- src/components/ItemGrid.js
+++ src/components/ItemGrid.js
@@ -1,46 +1,15 @@
-import React, {useEffect} from "react";
-import axios from "axios";
-import {message, Table} from "antd";
+import React from "react";
+import {Table} from "antd";
 import Counter from "./Counter";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {getAmountOfTodos, getTodos, isLoading} from "../redux/selectors/todoList";
-import {setLoading, setTodos} from "../redux/actions/todoList";
 
 const {Column} = Table;
 
-const fetchData = async dispatch => {
-    dispatch(setLoading(true));
-    axios("https://jsonplaceholder.typicode.com/todos")
-        .then(result => {
-            // on SUCCESS
-            dispatch(setTodos(result.data));
-        })
-        .catch(error => {
-            // on ERROR
-            message.error("Todos could not be fetched");
-            console.error(error);
-        })
-        .then(() => {
-            // at the END
-            dispatch(setLoading(false));
-        });
-};
-
-const cleanupData = dispatch => {
-    // Clean up state
-    dispatch(setTodos([]));
-    dispatch(setLoading(false));
-};
-
 const ItemGrid = () => {
-    const data = useSelector(getTodos),
-        amount = useSelector(getAmountOfTodos),
-        loading = useSelector(isLoading),
-        dispatch = useDispatch();
-    useEffect(() => {
-        fetchData(dispatch);
-        return cleanupData(dispatch);
-    }, [dispatch]);
+    const data = useSelector(getTodos);
+    const amount = useSelector(getAmountOfTodos);
+    const loading = useSelector(isLoading);
     return (
         <div className="ItemGrid">
             <Counter amount={amount}/>
