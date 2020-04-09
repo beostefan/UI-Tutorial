3600a3144e2499b7a4e4dd8d401f4215ded22088
Stefan Schmidt <schmidt@beontra.com>
2020-04-09 16:26:20 +0200

    Remove axios

--- src/redux/reducers/todoList.js
+++ src/redux/reducers/todoList.js
@@ -1,7 +1,22 @@
 import {types} from "../actions/todoList";
 
 const initialState = {
-    todos: []
+    todos: [
+        {
+            id: 1,
+            completed: false,
+            title: "Buy Easter Eggs"
+        },
+        {
+            id: 2,
+            completed: false,
+            title: "Make Toilet Paper Inventory"
+        },
+        {
+            id: 3,
+            completed: true,
+            title: "Work"
+        }]
 };
 
 const todoList = (state = initialState, action) => {
