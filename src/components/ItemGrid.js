baed86b837aa5839184e319200d801dde63a648b
Stefan Schmidt <schmidt@beontra.com>
2020-04-09 20:50:47 +0200

    Move axios request to TodoList component

--- src/components/ItemGrid.js
+++ src/components/ItemGrid.js
@@ -1,51 +1,20 @@
-import React, {useEffect} from "react";
-import axios from "axios";
-import {message, Table} from "antd";
+import React from "react";
+import {Table} from "antd";
 import Counter from "./Counter";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {getAmountOfTodos, getTodos, isLoading} from "../redux/selectors/todoList";
-import {setLoading, setTodos} from "../redux/actions/todoList";
 import {useHistory, useParams} from "react-router";
 import "./styles/ItemGrid.scss";
 
 const {Column} = Table;
 
-const fetchData = async dispatch => {
-    dispatch(setLoading(true));
-    axios("https://jsonplaceholder.typicode.com/todos")
-        .then(result => {
-            // on SUCCESS
-            dispatch(setTodos(result.data));
-        })
-        .catch(error => {
-            // on ERROR
-            message.error("Todos could not be fetched");
-            console.error(error);
-        })
-        .then(() => {
-            // at the END
-            dispatch(setLoading(false));
-        });
-};
-
-const cleanupData = dispatch => {
-    // Clean up state
-    dispatch(setTodos([]));
-    dispatch(setLoading(false));
-};
-
 const ItemGrid = () => {
-    const data = useSelector(getTodos),
-        amount = useSelector(getAmountOfTodos),
-        loading = useSelector(isLoading),
-        dispatch = useDispatch(),
-        history = useHistory();
+    const data = useSelector(getTodos);
+    const amount = useSelector(getAmountOfTodos);
+    const loading = useSelector(isLoading);
+    const history = useHistory();
     let {page} = useParams();
     page = page ? page : 1;
-    useEffect(() => {
-        fetchData(dispatch);
-        return cleanupData(dispatch);
-    }, [dispatch]);
     return (
         <div className="ItemGrid">
             <Counter amount={amount}/>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, {useEffect} from "react";
 import AddTodo from "./AddTodo";
 import ItemGrid from "./ItemGrid";
+import {setLoading, setTodos} from "../redux/actions/todoList";
+import axios from "axios";
+import {message} from "antd";
+import {useDispatch} from "react-redux";
+
+
+const fetchData = async dispatch => {
+    dispatch(setLoading(true));
+    axios("https://jsonplaceholder.typicode.com/todos")
+        .then(result => {
+            // on SUCCESS
+            dispatch(setTodos(result.data));
+        })
+        .catch(error => {
+            // on ERROR
+            message.error("Todos could not be fetched");
+            console.error(error);
+        })
+        .then(() => {
+            // at the END
+            dispatch(setLoading(false));
+        });
+};
+
+const cleanupData = dispatch => {
+    // Clean up state
+    dispatch(setTodos([]));
+    dispatch(setLoading(false));
+};
 
 const TodoList = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        fetchData(dispatch);
+        return cleanupData(dispatch);
+    }, [dispatch]);
     return (
         <div className="TodoList">
             <AddTodo/>
